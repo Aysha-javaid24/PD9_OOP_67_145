a0f56a6afc7f66b470858e5ea27352e28dc243cb
Aysha <ayshajavaid95@gmail.com>
2021-04-25 19:21:18 +0500

    updated
    Task4 updated

--- PD9_OOP_67_145/src/pd9_oop_67_145/Task4.java
+++ PD9_OOP_67_145/src/pd9_oop_67_145/Task4.java
@@ -70,9 +70,12 @@ public class Task4 {
              if(d == true){
              System.out.println("Correct input ");
          }  
-       else{
-        System.out.println("In correct input");
-          }    
+        case 4:
+           Scanner reader= new Scanner(System.in);
+           System.out.println("Enter Input :");
+            String s5 = reader.nextLine();
+            String s6=s5.toUpperCase();
+            System.out.println(s6);
             
             break;
             
@@ -84,13 +87,23 @@ public class Task4 {
     
         
     }
-
+ /*
+ *This function defines the method to check whether 
+ *the entered email is correct or not.
+ *@param mail(of string type)
+ *@return return type is boolean.
+ */
     
    public static boolean isValidEmail(String mail){
        String regex = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$/.";
        return mail.matches(regex);
    }
-   
+    /*
+ *This function defines the method to check whether 
+ *the entered substring ia part of string or not.
+ *@param S1(of string type)
+ *@return return type is boolean.
+ */
    public static boolean ContainMethod(String S1){
        String S2="a";
         boolean Contain = S1.contains(S2);
@@ -102,7 +115,12 @@ public class Task4 {
             return false;
         }
    }
-   
+ /*
+ *This function defines the method to check whether 
+ *the entered string is empty or not.
+ *@param S1(of string type)
+ *@return return type is boolean.
+ */
    public static boolean Empty(String S1){
        boolean empty = S1.isEmpty();
         if(empty){
@@ -113,7 +131,12 @@ public class Task4 {
             return false;
         }
    }
-   
+ /*
+ *This function defines the method to check whether 
+ *the entered start is correct or not.
+ *@param S1(of string type)
+ *@return return type is boolean.
+ */ 
    public static boolean StartWith(String S1){
        boolean SW = S1.startsWith("I");
         if(SW){
@@ -124,6 +147,17 @@ public class Task4 {
             return false;
         }
    }
-   
+ /*
+ *This function defines the method to convert a lowercase 
+ *string to uppercase string.
+ *@param s(of string type)
+ *@return return type is String.
+ */
+   public static String toUpperCase(String s)
+   {
+       String upperC;
+     upperC = s.toUpperCase();
+     return upperC;
+   }
    
 }    
